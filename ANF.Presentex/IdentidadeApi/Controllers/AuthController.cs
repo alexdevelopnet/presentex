a69f1c8db4d765d43f2eac333c80e0ef2c445b67
alexfnet2020 <alexfnet@gmail.com>
2022-05-08 18:22:18 -0300

    configurandoSwagger

--- ANF.Presentex/IdentidadeApi/Controllers/AuthController.cs
+++ ANF.Presentex/IdentidadeApi/Controllers/AuthController.cs
@@ -1,13 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static IdentidadeApi.Models.UserViewModels;
 
 namespace IdentidadeApi.Controllers
 {
-    [Route("")]
+    [ApiController]
+    [Route("api/identidade")]
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        private readonly SignInManager<IdentityUser> signInManager_;
+        private readonly UserManager<IdentityUser> userManager_;
+
+        public AuthController(SignInManager<IdentityUser> signInManager_, UserManager<IdentityUser> userManager_)
         {
-            return View();
+            this.signInManager_ = signInManager_;
+            this.userManager_ = userManager_;
         }
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+            var result = await userManager_.CreateAsync(user, usuarioRegistro.Senha);
+            if (result.Succeeded)
+            {
+                await signInManager_.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+     [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var result = await signInManager_.PasswordSignInAsync(userName: usuarioLogin.Email, password: usuarioLogin.Senha
+                , isPersistent: false, lockoutOnFailure: true);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
     }
 }
--- ANF.Presentex/IdentidadeApi/Startup.cs
+++ ANF.Presentex/IdentidadeApi/Startup.cs
@@ -1,18 +1,13 @@
 using IdentidadeApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IdentidadeApi
 {
@@ -25,7 +20,6 @@ namespace IdentidadeApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -37,11 +31,30 @@ namespace IdentidadeApi
             .AddDefaultTokenProviders();
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Presentex Identity API",
+                    Description = "Esta API faz parte do Sistema PresenteX .",
+                    Contact = new OpenApiContact() { Name = "ANFSUSAX", Email = "alexdevelopnet@gmail.com "},
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+
+            });
+
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
