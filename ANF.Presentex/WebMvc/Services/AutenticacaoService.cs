8adfb2e62a4315a029de38c44f8dd4edf21aac63
alexfnet2020 <alexfnet@gmail.com>
2022-05-10 18:03:45 -0300

    Serializando Objeto UsuarioRespostaLogin

--- ANF.Presentex/WebMvc/Services/AutenticacaoService.cs
+++ ANF.Presentex/WebMvc/Services/AutenticacaoService.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using static IdentidadeApi.Models.UserViewModels;
 
 namespace WebMvc.Services
 {
@@ -15,19 +16,21 @@ namespace WebMvc.Services
             this.httpClient_ = httpClient;
         }
 
-        public async Task<string> Login(UserViewModels.UsuarioLogin usuarioLogin)
+        public async Task<UsuarioRespostaLogin> Login(UserViewModels.UsuarioLogin usuarioLogin)
         {
             var loginContent = new StringContent(
                 content: JsonSerializer.Serialize(usuarioLogin),
                 Encoding.UTF8, mediaType: "application/Json");
             var response = await httpClient_.PostAsync(requestUri: "https://localhost:44344/api/identidade/autenticar/", loginContent);
 
-            var teste = await response.Content.ReadAsStringAsync();
-
-            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(),options);
         }
 
-        public async Task<string> Registro(UserViewModels.UsuarioRegistro usuarioRegistro)
+        public async Task<UsuarioRespostaLogin> Registro(UserViewModels.UsuarioRegistro usuarioRegistro)
         {
             var registroContent = new StringContent(
                 content: JsonSerializer.Serialize(usuarioRegistro),
@@ -36,7 +39,7 @@ namespace WebMvc.Services
 
             var teste = await response.Content.ReadAsStringAsync();
 
-            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync());
         }
     }
 }
