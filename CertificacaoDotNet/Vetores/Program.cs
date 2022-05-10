bbe0ee1d017990205291165f0c7664a9d8c91ef9
alexfnet2020 <alexfnet@gmail.com>
2022-05-09 23:46:00 -0300

    Trabalhando com Vetor

---
+++ CertificacaoDotNet/Vetores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor vetor = new Vetor(2);
+            try
+            {
+                vetor.Adiciona("elemento 1");
+                vetor.Adiciona("elemento 2");
+                vetor.Adiciona("elemento 3");
+            }
+            catch (Exception e)
+            { 
+                Console.WriteLine("{0}" , e.Message );
+            }
+            
+        }
+    }
+}
---
+++ CertificacaoDotNet/Vetores/Vetor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores
+{
+    public class Vetor
+    {
+        private String[] elementos;
+        private int tamanho;
+        public Vetor(int capacidade)
+        {
+            this.elementos = new String[capacidade];
+            this.tamanho = 0;
+        }
+        //public void Adiciona(string elemento)
+        //{
+        //    for (int i = 0; i < this.elementos.Length; i++)
+        //    {
+        //        if (this.elementos[i] == null)
+        //            this.elementos[i] = elemento;
+        //        break;
+        //    }
+        //}
+
+        public void Adiciona(string elemento)
+        {
+            if (this.tamanho < this.elementos.Length)
+            { 
+                this.elementos[this.tamanho] = elemento;
+                this.tamanho++;
+            }
+            else
+            {
+                throw new ArgumentException("Vetor j´´a está cheio");
+            }
+        }
+
+        public int Tamanho()
+        {
+            return this.tamanho;
+        }
+
+       
+
+    }
+}
